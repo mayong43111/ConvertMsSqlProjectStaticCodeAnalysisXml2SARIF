a3b74609fb8d8b946b6e2635b25af599f38f3f8f
Yong Ma <yongma@microsoft.com>
2022-03-09 11:57:30 +0800

    add sample 01

---
+++ examples/Sample01.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Users]
+(
+    UserID int NOT NULL,
+    UserName nvarchar(2) NOT NULL,
+    DisplayName nvarchar(255) NULL,
+    CountryID int NOT NULL,
+)
+WITH
+(
+    DISTRIBUTION = HASH (CountryID),
+    CLUSTERED COLUMNSTORE INDEX
+)
+GO
