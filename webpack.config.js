49d6fa7197ca19083408e609ad7a9cdfce4b67f5
Yong Ma <yongma@microsoft.com>
2022-03-13 13:49:27 +0800

    add npm publish

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './src/command/main.ts',
@@ -21,5 +22,8 @@ module.exports = {
                 loader: "ts-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true }),
+    ]
 };
